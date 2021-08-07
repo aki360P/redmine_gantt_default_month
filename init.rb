103a2251acab3e0904953da3e49711d5f2783fe3
aki360P <34680805+aki360P@users.noreply.github.com>
2021-08-07 16:38:10 +0900

    表示修正

--- init.rb
+++ init.rb
@@ -5,17 +5,12 @@ require 'rgdm_gantts_helper_patch'
 Redmine::Plugin.register :redmine_gantt_default_month do
   name 'Redmine gantt default month plugin'
   author 'Akinori Iwasaki'
-  description 'Preset gantt chart default month from'
-  version '0.1.0'
-  url 'https://github.com/aki360P/redmine_gantt_default_month_plugin'
+  description 'Preset default month to ganttchart'
+  version '1.0.0'
+  url 'https://github.com/aki360P/redmine_gantt_default_month'
   
-  #project_module :redmine_gantt_default_month do
-    permission :rgdm_setting, :rgdm_settings => [:edit]
-  #end
-  
-  
-  # add tab - project module
-  #menu :project_menu, :rgdm, {:controller => 'rgdm', :action => 'index' }, :caption => :label_rgdm, :param => :project_id
+
+  permission :rgdm_setting, :rgdm_settings => [:edit]
 
   # setting
   settings  partial: 'rgdm_global_settings/show',
