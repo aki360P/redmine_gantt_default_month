6c6eff9d0478da43a2435eaa5186b95404b6919a
aki360P <34680805+aki360P@users.noreply.github.com>
2022-09-13 22:26:32 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   #match 'projects/:project_id/settings/rgdm_setting/:action', :controller => 'rgdm_settings', :via => [:get, :post, :patch, :put]
 
   resources :projects do
-    patch '/settings/rgdm_setting/rgdm_setting', :controller => 'rgdm_settings', action: :edit
+    patch '/settings/rgdm_setting', :controller => 'rgdm_settings', action: :edit
   end
 end
